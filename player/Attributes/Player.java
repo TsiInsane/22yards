172cda8b2af9df146ef816e39850945af5da4082
Siddharth Shrotriya <siddharth@shrotriya.in>
2015-11-19 11:26:30 +0530

    Player class

---
+++ player/Attributes/Player.java
@@ -0,0 +1,96 @@
+package player.Attributes;
+
+public class Player
+{
+	int playerId;
+	String fName;
+	String lName;
+	int batting;
+	int bowling;
+	int keeping;
+	int fielding;
+	int fitness;
+	int form;
+	int experience;
+	
+	public int getPlayerId()
+	{
+		return playerId;
+	}
+	public void setPlayerId(int playerId)
+	{
+		this.playerId = playerId;
+	}
+	public String getfName()
+	{
+		return fName;
+	}
+	public void setfName(String fName)
+	{
+		this.fName = fName;
+	}
+	public String getlName()
+	{
+		return lName;
+	}
+	public void setlName(String lName)
+	{
+		this.lName = lName;
+	}
+	public int getBatting()
+	{
+		return batting;
+	}
+	public void setBatting(int batting) 
+	{
+		this.batting = batting;
+	}
+	public int getBowling() 
+	{
+		return bowling;
+	}
+	public void setBowling(int bowling) 
+	{
+		this.bowling = bowling;
+	}
+	public int getKeeping() 
+	{
+		return keeping;
+	}
+	public void setKeeping(int keeping) 
+	{
+		this.keeping = keeping;
+	}
+	public int getFielding() 
+	{
+		return fielding;
+	}
+	public void setFielding(int fielding) 
+	{
+		this.fielding = fielding;
+	}
+	public int getFitness() 
+	{
+		return fitness;
+	}
+	public void setFitness(int fitness) 
+	{
+		this.fitness = fitness;
+	}
+	public int getForm() 
+	{
+		return form;
+	}
+	public void setForm(int form) 
+	{
+		this.form = form;
+	}
+	public int getExperience() 
+	{
+		return experience;
+	}
+	public void setExperience(int experience) 
+	{
+		this.experience = experience;
+	}
+}
